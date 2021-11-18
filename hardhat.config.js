75af1f6eb4db7deffa87054a0f91b1600b434616
ezeev <evan.pease@gmail.com>
2021-11-17 16:58:07 -0800

    added dotenv instead of a secrets file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,20 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
+require('dotenv').config();
 
-const fs = require('fs')
-const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+// For deploying to mainnet
+const MAINNET_WALLET_PK = process.env.MAINNET_WALLET_PK
 
 // For deploying to testnet
-const INFURA_RINKEBY_URL = "https://rinkeby.infura.io/v3/2232dd82097143d09fc226844346895e"
-const RINKEBY_WALLET_PK = '0x6719d2a3437b78a2dc40948543f18f968812756cc3a7dd8b4c81dc8b9165b6ec'
+const INFURA_RINKEBY_URL = process.env.INFURA_RINKEBY_URL
+const RINKEBY_WALLET_PK = process.env.RINKEBY_WALLET_PK
 
 // For verifying a contract after it has been deployed on main or testnet
 // THIS IS NOT AUTOMATED. You must run the verify command (provided by the hardhat-etherscan plugin)
 // For example: hh verify --network rinkeby 0x4aBe3384567F2E82fA1B98DfD9D422f593d17651
-const ETHERSCAN_API_KEY = "Q4KYSIFYTUNS4C6WMJCD8JAIQ4XVKM4W4H"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
